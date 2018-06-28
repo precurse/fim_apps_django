af6a13ab7853299f2149f916a293e3aa4b1dea64
Andrew Klaus <andrewklaus@gmail.com>
2018-06-28 17:17:30 -0600

    Updating pipfile lock. Fixing import statements

--- fim_catalog/urls.py
+++ fim_catalog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import settings
+from django.conf import settings
 
 from . import views
 
